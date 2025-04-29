7abf4280b5d092480a93f12331d17334d40c720c
alex <alexthomas1349@gmail.com>
2025-04-29 00:50:40 -0400

    sdfsd

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,17 +6,28 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+    if(username){return true;}
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+    for(let i=0; i < users.length; i++){
+        if((users[i].username = username) && (users[i].password = password)){
+            return true;
+        }
+    }
+    return false;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let check = authenticatedUser(username, password);
+  if (check){
+    res.send('Welcome' + 'Username');
+  } else {
+    res.send('User doesnt exist!');
+  }
+    
 });
 
 // Add a book review
